2b3446d66ff518f0a8df1b571816a24a31205e02
Davs07 <botsymeisterspieler@gmail.com>
2024-09-22 19:09:07 -0500

    added route prueba and modified confirmacion page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { getConnection } from "./db.js";
+import path from 'path';  // Para manejar rutas de archivos
 
 const app = express();
 const PORT = 3333;
@@ -10,16 +11,31 @@ app.use(cors());
 app.use(express.json());
 
 // Servir archivos estáticos (como JavaScript, CSS, imágenes) desde la carpeta 'public'
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Rutas para servir las páginas HTML
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: './views' });
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: "./views" });
 });
 
-app.get('/confirmacion', (req, res) => {
-    res.sendFile('confirmacion.html', { root: './views' });
+app.get("/confirmacion", (req, res) => {
+  res.sendFile("confirmacion.html", { root: "./views" });
 });
+
+// Ruta para registrar la asistencia y mostrar HTML con mensaje de éxito
+app.get('/prueba/:id', async (req, res) => {
+    const idRegistro = req.params.id;
+
+    try {
+        await registrarAsistencia(idRegistro);
+        // Redirigir a la página de confirmación con el ID registrado
+        res.redirect(`/confirmacion?id=${idRegistro}`);
+    } catch (error) {
+        console.error('Error al registrar la asistencia:', error);
+        res.status(500).json({ success: false, message: 'Error al registrar la asistencia.' });
+    }
+});
+
 
 // Ruta para registrar asistencia
 app.post("/registrar-asistencia/:id", async (req, res) => {
@@ -41,8 +57,7 @@ async function registrarAsistencia(idRegistro) {
     const pool = await getConnection();
 
     // Ejecutar la consulta o procedimiento almacenado
-    const result = await pool.request()
-      .input("RegistroId", idRegistro) // Pasar el id como parámetro
+    const result = await pool.request().input("RegistroId", idRegistro) // Pasar el id como parámetro
       .query(`
                 INSERT INTO PruebaQR (RegistroId)
                 VALUES (@RegistroId)
